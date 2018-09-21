13ee1ceda10d7df2519e21f3f53532f33a74c74d
Marshall Slemp <marshall.slemp@gmail.com>
2018-09-20 19:21:37 -0500

    add fetch

--- src/App.js
+++ src/App.js
@@ -3,10 +3,19 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+
+  getQuote() {
+    fetch('https://api.chucknorris.io/jokes/random')
+      .then( res => res.json())
+      .then( res => console.log(res))
+  }
+
   render() {
+
     return (
       <div className="App">
-       
+        {this.getQuote()}
       </div>
     );
   }
